4a1c207d0f49d1abf00350900694014f0998db0a
Michael Mayer <michael@liquidbytes.net>
2020-01-11 15:05:28 +0100

    Refactor OSM package

--- internal/maps/osm/find.go
+++ internal/maps/osm/find.go
@@ -4,11 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/photoprism/photoprism-places/internal/maps/client"
 	"github.com/photoprism/photoprism-places/internal/s2"
-	"github.com/photoprism/photoprism-places/internal/util"
 )
 
 // API docs see https://wiki.openstreetmap.org/wiki/Nominatim#Reverse_Geocoding
@@ -54,54 +52,4 @@ func FindLocation(id string) (result Location, err error) {
 	result.ID = id
 
 	return result, nil
-}
-
-func (l Location) CellID() (result string) {
-	return l.ID
-}
-
-func (l Location) State() (result string) {
-	result = l.Address.State
-
-	return strings.TrimSpace(result)
-}
-
-func (l Location) City() (result string) {
-	if l.Address.City != "" {
-		result = l.Address.City
-	} else if l.Address.Town != "" {
-		result = l.Address.Town
-	} else if l.Address.Village != "" {
-		result = l.Address.Village
-	} else if l.Address.County != "" {
-		result = l.Address.County
-	} else if l.Address.State != "" {
-		result = l.Address.State
-	}
-
-	if len([]rune(result)) > 19 {
-		result = ""
-	}
-
-	return strings.TrimSpace(result)
-}
-
-func (l Location) Suburb() (result string) {
-	result = l.Address.Suburb
-
-	return strings.TrimSpace(result)
-}
-
-func (l Location) CountryCode() (result string) {
-	result = l.Address.CountryCode
-
-	return strings.ToLower(strings.TrimSpace(result))
-}
-
-func (l Location) Keywords() (result []string) {
-	return util.Keywords(l.LocDisplayName)
-}
-
-func (l Location) Source() string {
-	return ProviderName
 }
--- internal/maps/osm/location.go
+++ internal/maps/osm/location.go
@@ -1,5 +1,11 @@
 package osm
 
+import (
+	"strings"
+
+	"github.com/photoprism/photoprism-places/internal/util"
+)
+
 type Location struct {
 	ID             string  `json:"-"`
 	PlaceID        int     `json:"place_id"`
@@ -8,4 +14,54 @@ type Location struct {
 	LocType        string  `json:"type"`
 	LocDisplayName string  `json:"display_name"`
 	Address        Address `json:"address"`
+}
+
+func (l Location) CellID() (result string) {
+	return l.ID
+}
+
+func (l Location) State() (result string) {
+	result = l.Address.State
+
+	return strings.TrimSpace(result)
+}
+
+func (l Location) City() (result string) {
+	if l.Address.City != "" {
+		result = l.Address.City
+	} else if l.Address.Town != "" {
+		result = l.Address.Town
+	} else if l.Address.Village != "" {
+		result = l.Address.Village
+	} else if l.Address.County != "" {
+		result = l.Address.County
+	} else if l.Address.State != "" {
+		result = l.Address.State
+	}
+
+	if len([]rune(result)) > 19 {
+		result = ""
+	}
+
+	return strings.TrimSpace(result)
+}
+
+func (l Location) Suburb() (result string) {
+	result = l.Address.Suburb
+
+	return strings.TrimSpace(result)
+}
+
+func (l Location) CountryCode() (result string) {
+	result = l.Address.CountryCode
+
+	return strings.ToLower(strings.TrimSpace(result))
+}
+
+func (l Location) Keywords() (result []string) {
+	return util.Keywords(l.LocDisplayName)
+}
+
+func (l Location) Source() string {
+	return ProviderName
 }
